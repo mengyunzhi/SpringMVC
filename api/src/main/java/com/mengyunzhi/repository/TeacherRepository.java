95896986c81dd5e9a9b82a8fbd790c1ced5056d6
mskies <3792535@qq.com>
2017-04-06 14:33:31 +0800

    建立TeacherRepository接口

---
+++ api/src/main/java/com/mengyunzhi/repository/TeacherRepository.java
@@ -0,0 +1,9 @@
+package com.mengyunzhi.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by panjie on 17/4/6.
+ */
+public interface TeacherRepository extends CrudRepository<Teacher, Long> {
+}
